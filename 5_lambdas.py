082ea063e46e33e9c72eaaaba68adb582cc26eab
CosimoOne <mateusz.dyngosz@gmail.com>
2018-02-14 14:25:25 +0100

    another commit lambdas testing  from pycharm

---
+++ 5_lambdas.py
@@ -0,0 +1,12 @@
+# Filter method for even numbers
+from typing import List
+
+
+# def find_even_numbers(nums: list): #type declarations let you use types methods
+
+def find_even_numbers(nums: List[]): #type declarations with types [alt+enter] to import List let you use types methods
+
+# sort data
+
+data = [1, 9, -1, 20, 5, -100]
+
