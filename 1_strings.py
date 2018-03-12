64bfb379f1f0649775a2a6239483fd5e6bf9500b
CosimoOne <mateusz.dyngosz@gmail.com>
2018-03-12 22:42:32 +0100

    adding f style formating

--- 1_strings.py
+++ 1_strings.py
@@ -4,6 +4,7 @@ age = 30
 # not pythonic!
 print('Hi there ' + name + ' I see you are ' + str(age) + ' years old!')
 
+# p2 style
 print('Hi there %s I see you are %s years old!' % (name, age))
 
 print('Hi there {} I see you are {} years old!'.format(name, age))
@@ -14,3 +15,7 @@ data = {
 }
 
 print('Hi there {name} I see you are {age} years old!'.format(**data))
+
+# Python 3.6 only...
+print(f"HI there {name.upper()} I see you are {(str(age) + ' year' if age == 1 else str(age) + ' years')} old.")
+
