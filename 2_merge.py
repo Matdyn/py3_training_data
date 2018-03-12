7287e38a38040f51483be1a167b305f6ea9b5f98
CosimoOne <mateusz.dyngosz@gmail.com>
2018-03-12 22:50:25 +0100

    adding merge

---
+++ 2_merge.py
@@ -0,0 +1,37 @@
+# HTTP POST
+# /store/order/1?source=ad
+# { name: 'michael', cc: 293838383}
+#
+
+route = {'id': 1, 'action': 'order'}
+query = {'source': 'ad'}
+post = {'name': 'michael', 'cc': 293838383, 'id': 7}
+
+merged = {}
+
+# least pythonic (loops)
+for k, v in query.items():
+    merged[k] = v
+for k, v in post.items():
+    merged[k] = v
+for k, v in route.items():
+    merged[k] = v
+print(merged)
+
+# pythonic (update)
+merged = {}
+merged.update(query)
+merged.update(post)
+merged.update(route)
+print(merged)
+
+def fun(**kwargs):
+    print(kwargs)
+
+fun(name="Michael",id=7)
+fun(name="Mathew",id=6)
+
+
+# 3.5+ syntax:
+merged = {**query, **post, **route}
+print(merged)
